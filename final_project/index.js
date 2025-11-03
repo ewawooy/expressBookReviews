e30229d84207c8302d868cf42b7e63a180de3a94
Ella <ellamina3@gmail.com>
2025-11-03 12:32:32 -0500

    Fix some error in the index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const PORT = 5000;
 
+// Parse JSON and form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+// Session middleware
+app.use("/customer", session({
+  secret: "fingerprint_customer",
+  resave: true,
+  saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Auth middleware for protected routes
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if (req.session.authorization) {
+    const token = req.session.authorization.accessToken;
+    jwt.verify(token, "fingerprint_customer", (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
 });
- 
-const PORT =5000;
 
+// Mount routers
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running on port " + PORT));
